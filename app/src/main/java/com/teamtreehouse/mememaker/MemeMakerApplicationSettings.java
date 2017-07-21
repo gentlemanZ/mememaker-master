0e7e0c1fbc929914cc4a51cfccb59231b5fb9c98
Tianyang Zhong <tianyangzhong2013@gmail.com>
2017-07-20 22:53:00 -0400

    sharedPreferences with xml

--- app/src/main/java/com/teamtreehouse/mememaker/MemeMakerApplicationSettings.java
+++ app/src/main/java/com/teamtreehouse/mememaker/MemeMakerApplicationSettings.java
@@ -7,5 +7,19 @@ import android.preference.PreferenceManager;
 import com.teamtreehouse.mememaker.utils.StorageType;
 
 public class MemeMakerApplicationSettings {
+    SharedPreferences mSharedPreference;
+    public MemeMakerApplicationSettings(Context context){
+        mSharedPreference = PreferenceManager.getDefaultSharedPreferences(context);
+    }
 
+    public String getStoragePreference(){
+        return mSharedPreference.getString("Storage",StorageType.INTERNAL);
+    }
+
+    public void setSharedPreference(String storageType){
+        mSharedPreference
+                .edit()
+                .putString("Storage",storageType)
+                .apply();
+    }
 }
