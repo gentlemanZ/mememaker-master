6934cd70d7624818c50bf9f1c80b8db55d1c558b
Tianyang Zhong <tianyangzhong2013@gmail.com>
2017-07-25 20:30:00 -0400

    update sqlite set up

--- app/src/main/java/com/teamtreehouse/mememaker/database/MemeDataSource.java
+++ app/src/main/java/com/teamtreehouse/mememaker/database/MemeDataSource.java
@@ -14,9 +14,12 @@ import java.util.ArrayList;
 public class MemeDataSource {
 
     private Context mContext;
-
+    private MemeSQLiteHelper mMemeSqliteHelper;
     public MemeDataSource(Context context) {
         mContext = context;
+        mMemeSqliteHelper = new MemeSQLiteHelper(context);
+        SQLiteDatabase database = mMemeSqliteHelper.getReadableDatabase();
+        database.close();
     }
 }
 
